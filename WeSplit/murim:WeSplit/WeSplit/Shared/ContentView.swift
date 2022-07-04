011d91365f1d865e20566ec10cd1189a4e864fbf
nora <anes2354@gmail.com>
2022-07-04 14:00:03 +0900

    [chore]: create project

---
+++ WeSplit/murim:WeSplit/WeSplit/Shared/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Shared
+//
+//  Created by TOBO WORLD on 2022/07/04.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
