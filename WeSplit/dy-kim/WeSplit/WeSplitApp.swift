278663c915c36a43d9d82c787c1ab5bc2c1f18de
Daeyoung Kim <daykim22@gmail.com>
2022-07-04 20:57:21 +0900

    WeSplit: Finished

---
+++ WeSplit/dy-kim/WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by 김대영 on 2022/06/30.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
