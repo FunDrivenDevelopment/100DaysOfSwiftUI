c009cd40c64d1028ea52470b62e23538727b1cd0
nora <anes2354@gmail.com>
2022-07-04 16:12:23 +0900

    [feat]: study two-way binding

--- WeSplit/murim:WeSplit/WeSplit/Shared/ContentView.swift
+++ WeSplit/murim:WeSplit/WeSplit/Shared/ContentView.swift
@@ -8,16 +8,30 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State var name = ""
-
        var body: some View {
-           Form {
-               TextField("Enter your name", text: $name)
-               Text("Hello, world!")\    Text("Your name is \(name)")
-           }
-       
+           TipPercent()
        }
+}
+
+struct TipPercent : View {
+    @State private var checkAmount = 0.0
+    @State private var numberOfPeople = 2
+    @State private var tipPercentage = 20
     
+    let tipPercentages = [10, 15, 20, 25, 0]
+
+    var body: some View {
+        Form {
+            Section {
+                TextField("Amount", value: $checkAmount, format: .currency(code: Locale.current.currencyCode ?? "USD"))
+                    .keyboardType(.decimalPad)
+            }
+
+            Section {
+                Text(checkAmount, format: .currency(code: Locale.current.currencyCode ?? "USD"))
+            }
+        }
+    }
  
 }
 
