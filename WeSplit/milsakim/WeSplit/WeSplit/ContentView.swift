4ba6f2b8c2a128e667c181e64fa643baf9e782e6
milsakim <hyejeekim@me.com>
2022-07-05 10:49:03 +0900

    feat: 메인 화면 구성 및 가격 계산 기능 추가

--- WeSplit/milsakim/WeSplit/WeSplit/ContentView.swift
+++ WeSplit/milsakim/WeSplit/WeSplit/ContentView.swift
@@ -8,9 +8,54 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var checkAmount: Double = 0
+    @State private var numberOfPeople: Int = 0
+    @State private var tipPercentage: Int = 20
+    
+    let tipPercentages: [Int] = [10, 15, 20, 25, 0]
+    
+    var totalPerPerson: Double {
+        let peopleCount: Double = Double(numberOfPeople + 2)
+        let tipSelection: Double = Double(tipPercentage)
+        
+        let tipValue: Double = checkAmount * tipSelection / 100
+        let grandTotal: Double = checkAmount + tipValue
+        let totalPerPerson: Double = grandTotal / peopleCount
+        
+        return totalPerPerson
+    }
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            Form {
+                Section {
+                    TextField("Amount", value: $checkAmount, format: .currency(code: Locale.current.currencyCode ?? "USD"))
+                        .keyboardType(.decimalPad)
+                    
+                    Picker("Number of People", selection: $numberOfPeople) {
+                        ForEach(2..<100) {
+                            Text("\($0) people")
+                        }
+                    }
+                }
+                
+                Section {
+                    Picker("Tip percentage", selection: $tipPercentage) {
+                        ForEach(tipPercentages, id: \.self) {
+                            Text($0, format: .percent)
+                        }
+                    }
+                    .pickerStyle(.segmented)
+                } header: {
+                    Text("How much tip do you want to leave?")
+                }
+                
+                Section {
+                    Text(totalPerPerson, format: .currency(code: Locale.current.currencyCode ?? "USD"))
+                }
+            }
+            .navigationTitle("WeSplit")
+        }
     }
 }
 
