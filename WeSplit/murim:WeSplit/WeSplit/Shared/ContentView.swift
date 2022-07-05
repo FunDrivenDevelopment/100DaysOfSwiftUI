11ff74a130fc7f1ea684b17faff819060d6514c6
nora <anes2354@gmail.com>
2022-07-05 18:44:37 +0900

    [feat]: solve challenge

--- WeSplit/murim:WeSplit/WeSplit/Shared/ContentView.swift
+++ WeSplit/murim:WeSplit/WeSplit/Shared/ContentView.swift
@@ -8,37 +8,81 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var checkAmount : Double = 0.0
+    @State private var numberOfPeople = 2
+    @State private var tipPercentage : Int = 20
+    
+    @FocusState private var amountIsFocused : Bool
+    
+    let tipPercentages = [10,15,20,25,0]
+    
+    var totalPerPerson : Double {
+        let peopleCount = Double(numberOfPeople + 2)
+        let tipSelection = Double(tipPercentage)
+        
+        let tipValue : Double = checkAmount / 100 * Double(tipPercentage)
+        let grandTotal = checkAmount + tipValue
+        let amountPerPerson = grandTotal/peopleCount
+        
+        return amountPerPerson
+    }
+    
        var body: some View {
-           TipPercent()
+           NavigationView {
+               Form {
+                   Section{
+                       TextField("Amount", value: $checkAmount, format: .currency(code: Locale.current.currencyCode ?? "USD"))
+                               .keyboardType(.decimalPad)
+                               .focused($amountIsFocused)
+
+                           Picker("Number of people", selection: $tipPercentage) {
+                               ForEach(2 ..< 100) {
+                                   Text("\($0) people")
+                               }
+                               .pickerStyle(.segmented)
+                           }
+                   }header: {
+                       Text("How much tip do you want to leave?")
+                   }
+                   Section {
+                       Text(checkAmount, format: .currency(code: Locale.current.currencyCode ?? "USD"))
+                   }
+                   thirdSection()
+               }
+               .navigationTitle("We Split")
+               .toolbar{
+                   ToolbarItemGroup(placement: .keyboard) {
+                       Spacer()
+                       Button("Done"){
+                           amountIsFocused = false
+                       }
+                   }
+               }
+           }
        }
 }
 
-struct TipPercent : View {
-    @State private var checkAmount = 0.0
-    @State private var numberOfPeople = 2
-    @State private var tipPercentage = 20
-    
-    let tipPercentages = [10, 15, 20, 25, 0]
-
+struct thirdSection : View {
+   
     var body: some View {
-        Form {
-            Section {
-                TextField("Amount", value: $checkAmount, format: .currency(code: Locale.current.currencyCode ?? "USD"))
-                        .keyboardType(.decimalPad)
+        Section {
+            anotherView()
+        } header: {
+            Text("Amount per person")
+        }
+    }
+}
 
-                    Picker("Number of people", selection: $numberOfPeople) {
-                        ForEach(2 ..< 100) {
-                            Text("\($0) people")
-                        }
-                        .pickerStyle(.segmented)
-                    }
-            }
-            Section {
-                Text(checkAmount, format: .currency(code: Locale.current.currencyCode ?? "USD"))
-            } header: {
-                Text("How much tip do you want to leave?")
+struct anotherView : View {
+    @State var selectPercent = 0
+    
+    var body: some View {
+        Picker("tip percentage", selection: $selectPercent) {
+            ForEach(0..<101) {
+                Text("\($0) %")
             }
         }
+        .pickerStyle(.wheel)
     }
 }
 
