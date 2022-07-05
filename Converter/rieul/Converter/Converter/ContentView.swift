ce275738d43ad3f4ca51dbe761422d2d6c55ec45
JiWon Lee <jiwon.swdev@gmail.com>
2022-07-05 21:42:58 +0900

    feat: Converter init

---
+++ Converter/rieul/Converter/Converter/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Converter
+//
+//  Created by 이지원 on 2022/07/05.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
