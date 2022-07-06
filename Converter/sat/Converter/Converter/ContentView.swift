6a9d3961cbda178704d30d276bf235127d1e5723
Yeonu Yi <yeonu0616@gmail.com>
2022-07-06 21:44:11 +0100

    🎉 Finish converter challenge

--- Converter/sat/Converter/Converter/ContentView.swift
+++ Converter/sat/Converter/Converter/ContentView.swift
@@ -9,6 +9,57 @@ import SwiftUI
 
 struct ContentView: View {
 
+    // MARK: - Declaration
+
+    private enum Unit {
+        case celsius(value: Double)
+        case fahrenheit(value: Double)
+        case kelvin(value: Double)
+
+        init(unit: String, value: Double) {
+            if unit == "Celsius" {
+                self = .celsius(value: value)
+            } else if unit == "Fahrenheit" {
+                self = .fahrenheit(value: value)
+            } else {
+                self = .kelvin(value: value)
+            }
+        }
+
+        var toCelsius: Double {
+            switch self {
+            case .celsius(let value):
+                return value
+            case .fahrenheit(let value):
+                return (value - 32) * 5/9
+            case .kelvin(let value):
+                return value - 273.15
+            }
+        }
+
+        var toFahrenheit: Double {
+            switch self {
+            case .celsius(let value):
+                return (value * 9/5) + 32
+            case .fahrenheit(let value):
+                return value
+            case .kelvin(let value):
+                return (value - 273.15) * 9/5 + 32
+            }
+        }
+
+        var toKelvin: Double {
+            switch self {
+            case .celsius(let value):
+                return value + 273.15
+            case .fahrenheit(let value):
+                return (value - 32) * 5/9 + 273.15
+            case .kelvin(let value):
+                return value
+            }
+        }
+    }
+
     // MARK: - Properties
 
     private let units = ["Celsius", "Fahrenheit", "Kelvin"]
@@ -16,10 +67,21 @@ struct ContentView: View {
     @State private var fromUnit: String = "Celsius"
     @State private var toUnit: String = "Fahrenheit"
     @State private var fromValue: Double = 0.0
-    @State private var toValue: Double = 0.0
+
+    private var toValue: Double {
+        let from = Unit(unit: fromUnit, value: fromValue)
+
+        if toUnit == "Celsius" {
+            return from.toCelsius
+        } else if toUnit == "Fahrenheit" {
+            return from.toFahrenheit
+        } else {
+            return from.toKelvin
+        }
+    }
 
     // MARK: - Body
-    
+
     var body: some View {
         NavigationView {
             Form {
@@ -42,8 +104,7 @@ struct ContentView: View {
                     .frame(maxWidth: .infinity, alignment: .center)
 
                 Section {
-                    TextField("To", value: $toValue, format: .number)
-                        .keyboardType(.decimalPad)
+                    Text(toValue, format: .number)
 
                     Picker("Unit", selection: $toUnit) {
                         ForEach(units, id: \.self) {
