39ea7e182c1a6e6fa018faec696c69db63b5e12e
Yeonu Yi <yeonu0616@gmail.com>
2022-07-06 21:19:25 +0100

    ✨ Set UI

--- Converter/sat/Converter/Converter/ContentView.swift
+++ Converter/sat/Converter/Converter/ContentView.swift
@@ -8,9 +8,55 @@
 import SwiftUI
 
 struct ContentView: View {
+
+    // MARK: - Properties
+
+    private let units = ["Celsius", "Fahrenheit", "Kelvin"]
+
+    @State private var fromUnit: String = "Celsius"
+    @State private var toUnit: String = "Fahrenheit"
+    @State private var fromValue: Double = 0.0
+    @State private var toValue: Double = 0.0
+
+    // MARK: - Body
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            Form {
+                Section {
+                    TextField("From", value: $fromValue, format: .number)
+                        .keyboardType(.decimalPad)
+
+                    Picker("Unit", selection: $fromUnit) {
+                        ForEach(units, id: \.self) {
+                            Text($0)
+                        }
+                    }
+                    .pickerStyle(.segmented)
+                } header: {
+                    Text("From")
+                }
+
+                Image(systemName: "arrow.up.arrow.down")
+                    .font(Font.system(.largeTitle))
+                    .frame(maxWidth: .infinity, alignment: .center)
+
+                Section {
+                    TextField("To", value: $toValue, format: .number)
+                        .keyboardType(.decimalPad)
+
+                    Picker("Unit", selection: $toUnit) {
+                        ForEach(units, id: \.self) {
+                            Text($0)
+                        }
+                    }
+                    .pickerStyle(.segmented)
+                } header: {
+                    Text("To")
+                }
+            }
+            .navigationTitle("Converter")
+        }
     }
 }
 
