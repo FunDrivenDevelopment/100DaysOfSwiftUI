c50025caca785115bb20daa957cf2a301e42747f
Yeonu Yi <yeonu0616@gmail.com>
2022-07-06 18:04:25 +0100

    🎉 Create Converter project

---
+++ Converter/sat/Converter/Converter/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Converter
+//
+//  Created by Yeonu Yi on 06/07/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
