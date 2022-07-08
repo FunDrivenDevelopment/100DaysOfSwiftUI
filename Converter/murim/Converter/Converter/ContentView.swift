f8825d7558842e5a00c83b4935f50bcefd3f9d39
nora <anes2354@gmail.com>
2022-07-08 16:45:10 +0900

    [feat]: study UnitLength

--- Converter/murim/Converter/Converter/ContentView.swift
+++ Converter/murim/Converter/Converter/ContentView.swift
@@ -8,12 +8,71 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var input = 100.0
+    @State private var inputUnit = UnitLength.meters
+    @State private var outputUnit = UnitLength.kilometers
+    
+    @FocusState private var inputIsFocused : Bool
+    
+    let units : [UnitLength] = [.feet,.kilometers,.meters,.miles,.yards]
+    
+    let formatter : MeasurementFormatter
+    
+    var result : String {
+        let inputMeasurement = Measurement(value: input, unit: inputUnit)
+        let outputMeasurement = inputMeasurement.converted(to: outputUnit)
+        
+        return formatter.string(from: outputMeasurement)
+    }
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView{
+            Form{
+          
+                Section {
+                    TextField("Amount",value : $input,format: .number)
+                        .keyboardType(.decimalPad)
+                        .focused($inputIsFocused)
+                }header: {
+                    Text("Amount to convert")
+                }
+                
+                Picker("Convert from", selection: $inputUnit){
+                    ForEach(units, id : \.self){
+                        Text(formatter.string(from: $0).capitalized)
+                    }
+                }
+                Picker("Convert to", selection: $outputUnit){
+                    ForEach(units, id : \.self){
+                        Text(formatter.string(from: $0).capitalized)
+                    }
+                }
+                
+                Section{
+                    Text(result)
+                }header: {
+                    Text("Result")
+                }
+            }
+            .navigationTitle("Converter")
+            .toolbar{
+                ToolbarItemGroup(placement: .keyboard){
+                    Spacer()
+                    
+                    Button("Done"){
+                        inputIsFocused = false
+                    }
+                }
+            }
+        }
+    }
+    init(){
+        formatter = MeasurementFormatter()
+        formatter.unitOptions = .providedUnit
+        formatter.unitStyle = .long
     }
 }
-
+
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
