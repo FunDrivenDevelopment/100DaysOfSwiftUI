0afbffbee2a6e446f2dd7dc7ba5bc57ab6222f6c
JiWon Lee <jiwon.swdev@gmail.com>
2022-07-10 22:02:01 +0900

    wrong flag

--- GuesstheFlag/rieul/GuessTheFlag/GuessTheFlag/ContentView.swift
+++ GuesstheFlag/rieul/GuessTheFlag/GuessTheFlag/ContentView.swift
@@ -71,7 +71,7 @@ struct ContentView: View {
             scoreTitle = "Correct"
             score += 1
         } else {
-            scoreTitle = "Wrong"
+            scoreTitle = "Wrong! That’s the flag of \(countries[number])"
         }
 
         showingScore = true
