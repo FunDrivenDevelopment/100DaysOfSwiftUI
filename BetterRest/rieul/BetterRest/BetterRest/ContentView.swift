7e6f9baf1222ac923f07286694a0da768f125286
JiWon Lee <jiwon.swdev@gmail.com>
2022-07-17 00:53:48 +0900

    connecting swiftui to core ml

--- BetterRest/rieul/BetterRest/BetterRest/ContentView.swift
+++ BetterRest/rieul/BetterRest/BetterRest/ContentView.swift
@@ -5,12 +5,17 @@
 //  Created by 이지원 on 2022/07/16.
 //
 
+import CoreML
 import SwiftUI
 
 struct ContentView: View {
     @State private var wakeUp: Date = .now
     @State private var sleepAmount: Double = 8.0
     @State private var coffeeAmount: Int = 1
+
+    @State private var alertTitle: String = ""
+    @State private var alertMessage: String = ""
+    @State private var showingAlert: Bool = false
 
     var body: some View {
         NavigationView {
@@ -48,11 +53,40 @@ struct ContentView: View {
             .toolbar {
                 Button("Calculate", action: calculateBedtime)
             }
+            .alert(alertTitle, isPresented: $showingAlert) {
+                Button("OK") { }
+            } message: {
+                Text(alertMessage)
+            }
         }
     }
 
     func calculateBedtime() {
+        do {
+            let config: MLModelConfiguration = MLModelConfiguration()
+            let model: SleepCalculator = try SleepCalculator(configuration: config)
 
+            let components: DateComponents = Calendar.current.dateComponents(
+                [.hour, .minute],
+                from: wakeUp
+            )
+            let hour: Int = (components.hour ?? 0) * 60 * 60
+            let minute: Int = (components.minute ?? 0) * 60
+
+            let prediction = try model.prediction(
+                wake: Double(hour + minute),
+                estimatedSleep: sleepAmount,
+                coffee: Double(coffeeAmount)
+            )
+
+            let sleepTime: Date = wakeUp - prediction.actualSleep
+            alertTitle = "Your ideal bedtime is..."
+            alertMessage = sleepTime.formatted(date: .omitted, time: .shortened)
+        } catch {
+            alertTitle = "Error"
+            alertMessage = "Sorry, there was a problem calculating you bedtime"
+            showingAlert = true
+        }
     }
 }
 
