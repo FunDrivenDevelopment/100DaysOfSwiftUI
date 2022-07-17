b12086a35514fe26e1dd4497e18dc59315c14e68
Yeonu Yi <yeonu0616@gmail.com>
2022-07-17 14:12:55 +0100

    ✨ Create Title modifier

--- ViewsAndModifiers/sat/ViewsAndModifiers/ViewsAndModifiers/ContentView.swift
+++ ViewsAndModifiers/sat/ViewsAndModifiers/ViewsAndModifiers/ContentView.swift
@@ -7,10 +7,25 @@
 
 import SwiftUI
 
+struct Title: ViewModifier {
+
+    func body(content: Content) -> some View {
+        content
+            .font(.largeTitle)
+            .foregroundColor(.blue)
+    }
+}
+
+extension View {
+    func titleStyle() -> some View {
+        modifier(Title())
+    }
+}
+
 struct ContentView: View {
     var body: some View {
         Text("Hello, world!")
-            .padding()
+            .titleStyle()
     }
 }
 
