1eca8f89e2e323a11dc414caf77eccb7d9c60137
Daeyoung Kim <daykim22@gmail.com>
2022-07-24 18:12:16 +0900

    프로젝트 & 챌린지 완료

--- BetterRest/dy-kim/BetterRest/BetterRest/ContentView.swift
+++ BetterRest/dy-kim/BetterRest/BetterRest/ContentView.swift
@@ -11,26 +11,45 @@ import SwiftUI
 struct ContentView: View {
     @State private var wakeUp = defaultWakeTime
     @State private var sleepAmount: Double = 8
-    @State private var coffeeAmount: Int = 2
+    @State private var coffeeAmount: Int = 3
 
     @State private var alertTitle = ""
     @State private var alertMessage = ""
     @State private var showingAlert = false
+    @State private var getCoffeeAmountWithPicker = false
+    @State private var autoCalculation = false
+
+    private var bedTimeString: String {
+        guard let bedTime = calculateBedtime() else {
+            return "Error!"
+        }
+
+        return bedTime.formatted(date: .omitted, time: .shortened)
+    }
+    private let coffeeAmountRange = 0...20
 
     var body: some View {
-        VStack(spacing: 0) {
-            Form {
-                desiredWakeTime
-                desiredSleepAmountSection
-                coffeeAmountSection
+        NavigationView {
+            VStack(spacing: 0) {
+                Form {
+                    desiredWakeTime
+                    desiredSleepAmountSection
+                    coffeeAmountSection
+                    Toggle("자동 계산", isOn: $autoCalculation)
+                }
+
+                if autoCalculation {
+                    bedTimeView
+                } else {
+                    calculateButton
+                }
             }
-            calculateButton
-        }
-        .ignoresSafeArea(edges: .bottom)
-        .alert(alertTitle, isPresented: $showingAlert) {
-            Button("알겠어요") { }
-        } message: {
-            Text(alertMessage)
+            .alert(alertTitle, isPresented: $showingAlert) {
+                Button("알겠어요") { }
+            } message: {
+                Text(alertMessage)
+            }
+            .navigationTitle("언제 자야할까?")
         }
     }
 
@@ -38,6 +57,7 @@ struct ContentView: View {
         HStack {
             Text("언제 일어나고 싶으신가요?")
                 .font(.headline)
+                .padding(.vertical)
 
             Spacer()
 
@@ -57,26 +77,71 @@ struct ContentView: View {
 
     var coffeeAmountSection: some View {
         Section {
-            Stepper("\(coffeeAmount.formatted()) 컵", value: $coffeeAmount, in: 1...20)
+            if getCoffeeAmountWithPicker {
+                Picker("커피 얼마나?", selection: $coffeeAmount) {
+                    ForEach(coffeeAmountRange, id: \.self) {
+                        Text("\($0) 잔")
+                    }
+                }.pickerStyle(.wheel)
+            } else {
+                Stepper("\(coffeeAmount.formatted()) 컵", value: $coffeeAmount, in: coffeeAmountRange)
+            }
         } header: {
             Text("하루에 커피 몇 잔을 드시나요?")
                 .font(.headline).foregroundColor(.primary)
+        } footer: {
+            Toggle("Use Picker?", isOn: $getCoffeeAmountWithPicker)
+                .toggleStyle(UnobtrusiveToggleStyle())
+                .font(.headline)
         }
+    }
+
+    var bedTimeView: some View {
+        VStack {
+            Text("추천 취침 시각")
+                .foregroundStyle(.thickMaterial)
+
+            Text(bedTimeString)
+                .font(.largeTitle)
+                .bold()
+                .foregroundColor(.white)
+        }
+        .frame(maxWidth: .infinity)
+        .padding(.top)
+        .ignoresSafeArea(edges: .bottom)
+        .background(.indigo)
     }
 
     var calculateButton: some View {
-        Button(action: calculateBedtime) {
+        Button(action: showBedTime) {
             Text("취침 시각 계산")
                 .foregroundColor(.white)
+                .font(.title2)
                 .fontWeight(.heavy)
                 .frame(maxWidth: .infinity)
-                .padding(20)
-                .padding(.bottom)
+                .padding(.top, 30)
                 .background(.primary)
         }
+        .foregroundColor(.indigo)
     }
 
-    func calculateBedtime() {
+    func showBedTime() {
+        guard let bedTime = calculateBedtime() else {
+            alertTitle = "에러!"
+            alertMessage = "취침 시각 계산 도중에 에러가 발생했어요."
+            showingAlert = true
+
+            return
+        }
+
+        alertTitle = "추천 취침 시간을 알려드려요"
+        alertMessage = bedTime.formatted(date: .omitted, time: .shortened)
+        showingAlert = true
+    }
+
+    func calculateBedtime() -> Date? {
+        var bedTime: Date? = Date()
+
         do {
             let config = MLModelConfiguration()
             let model = try BedtimeCalculator(configuration: config)
@@ -87,28 +152,40 @@ struct ContentView: View {
 
             let prediction = try model.prediction(wake: Double(hour + minute), estimatedSleep: sleepAmount, coffee: Double(coffeeAmount))
 
-            let bedTime = wakeUp - prediction.actualSleep
-
-            alertTitle = "추천 취침 시간을 알려드려요"
-            alertMessage = bedTime.formatted(date: .omitted, time: .shortened)
+            bedTime = wakeUp - prediction.actualSleep
         } catch {
-            alertTitle = "에러!"
-            alertMessage = "취침 시각 계산 도중에 에러가 발생했어요."
+            bedTime = nil
         }
-
-        showingAlert = true
+        return bedTime
     }
-}
 
-var defaultWakeTime: Date {
-    var components = DateComponents()
-    components.hour = 7
-    components.minute = 0
-    return Calendar.current.date(from: components) ?? Date.now
+    static var defaultWakeTime: Date {
+        var components = DateComponents()
+        components.hour = 7
+        components.minute = 0
+        return Calendar.current.date(from: components) ?? Date.now
+    }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+struct UnobtrusiveToggleStyle: ToggleStyle {
+    func makeBody(configuration: Self.Configuration) -> some View {
+        HStack {
+            Spacer()
+
+            configuration.label.padding(5)
+
+            Image(systemName: configuration.isOn ? "checkmark.circle.fill" : "circle")
+                .foregroundColor(configuration.isOn ? .accentColor : .gray)
+                .font(.headline.weight(.heavy))
+                .onTapGesture {
+                    configuration.isOn.toggle()
+                }
+        }
     }
 }
