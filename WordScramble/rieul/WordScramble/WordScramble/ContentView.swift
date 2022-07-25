a8c789164d2252d82a85a45cb6560918236a72cc
JiWon Lee <jiwon.swdev@gmail.com>
2022-07-25 22:50:36 +0900

    adding to a list of words

--- WordScramble/rieul/WordScramble/WordScramble/ContentView.swift
+++ WordScramble/rieul/WordScramble/WordScramble/ContentView.swift
@@ -8,18 +8,46 @@
 import SwiftUI
 
 struct ContentView: View {
-    let people = ["Finn", "Leia", "Luke", "Rey"]
+    /// 이미 사용한 단어 배열
+    @State private var usedWords: [String] = []
+    /// 다른 단어의 철자에 대한 루트 단어
+    @State private var rootWord: String = ""
+    /// 텍스트 필드에 바인딩할 수 있는 문자열
+    @State private var newWord: String = ""
 
     var body: some View {
-        List {
-            Text("Static Row")
+        NavigationView {
+            List {
+                Section {
+                    TextField("Enter your word", text: $newWord)
+                        .textInputAutocapitalization(.never)
+                }
 
-            ForEach(people, id: \.self) {
-                Text($0)
+                Section {
+                    ForEach(usedWords, id: \.self) { word in
+                        HStack {
+                            Image(systemName: "\(word.count).circle")
+                            Text(word)
+                        }
+                    }
+                }
             }
-
-            Text("Static Row")
+            .navigationTitle(rootWord)
+            .onSubmit(addNewWord)
         }
+    }
+
+    private func addNewWord() {
+        let answer: String = newWord.lowercased().trimmingCharacters(in: .whitespacesAndNewlines)
+
+        guard !answer.isEmpty else {
+            return
+        }
+
+        withAnimation {
+            usedWords.insert(answer, at: 0)
+        }
+        newWord = ""
     }
 }
 
