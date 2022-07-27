28871f8bb40e5dc8dea4c0fe38343e3dad2e7250
Yeonu Yi <yeonu0616@gmail.com>
2022-07-27 17:41:39 +0100

    🎉 Create BetterRest project

---
+++ BetterRest/sat/BetterRest/BetterRest/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  BetterRest
+//
+//  Created by Yeonu Yi on 27/07/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
