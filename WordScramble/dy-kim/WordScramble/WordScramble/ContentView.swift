1916241d978ff55b8277814ba7d0cce8715e9fdf
Daeyoung Kim <daykim22@gmail.com>
2022-07-28 21:39:21 +0900

    ContentView 등장할 때 텍스트 파일 읽어서 랜덤한 단어 불러오기 구현

--- WordScramble/dy-kim/WordScramble/WordScramble/ContentView.swift
+++ WordScramble/dy-kim/WordScramble/WordScramble/ContentView.swift
@@ -8,9 +8,35 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var rootWord = ""
+    @State private var newWord = ""
+
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            List {
+                Section {
+                    TextField("Type new word", text: $newWord)
+                        .textInputAutocapitalization(.never)
+                }
+            }
+            .navigationTitle(rootWord)
+        }
+        .onAppear(perform: startGame)
+    }
+
+    func startGame() {
+        // Read start.txt
+        // Pick random word and assign it to the rootWord
+        if let startWordsURL = Bundle.main.url(forResource: "start", withExtension: "txt") {
+            if let startWords = try? String(contentsOf: startWordsURL) {
+                let allWords = startWords.components(separatedBy: "\n")
+                rootWord = allWords.randomElement() ?? "silkworm"
+
+                return
+            }
+        }
+
+        fatalError("Can not load 'start.txt' file from bundle!")
     }
 }
 
