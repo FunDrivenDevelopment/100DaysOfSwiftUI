b9aeb537d89c15513f3e8ef692ead35a74b6dce3
Daeyoung Kim <daykim22@gmail.com>
2022-07-28 14:38:08 +0900

    Project #5 시작!

---
+++ WordScramble/dy-kim/WordScramble/WordScramble/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  WordScramble
+//
+//  Created by 김대영 on 2022/07/28.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
