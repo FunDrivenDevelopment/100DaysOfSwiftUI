5e003008947d2094ab52119484d724505e5e093b
milsakim <hyejeekim@me.com>
2022-07-28 21:12:34 +0900

    feat[day24]: challenge 3번 완료

--- ViewsAndModifiers/milsakim/ViewsAndModifiers/ViewsAndModifiers/ContentView.swift
+++ ViewsAndModifiers/milsakim/ViewsAndModifiers/ViewsAndModifiers/ContentView.swift
@@ -7,10 +7,51 @@
 
 import SwiftUI
 
-struct ContentView: View {
+struct ProminentTitle: ViewModifier {
+    func body(content: Content) -> some View {
+        content
+            .font(.largeTitle)
+            .foregroundColor(.blue)
+    }
+}
+
+extension View {
+    func prominentTitle() -> some View {
+        modifier(ProminentTitle())
+    }
+}
+
+struct CapsuleText: View {
+    var text: String
+    
     var body: some View {
-        Text("Hello, world!")
+        Text(text)
             .padding()
+            .background(.blue)
+            .clipShape(Capsule())
+    }
+}
+
+struct ContentView: View {
+    @State private var useRedText: Bool = false
+    
+    let motto1: Text = Text("Motto 1")
+    let motto2: Text = Text("Motto 2")
+    
+    var body: some View {
+        VStack {
+            motto1
+                .foregroundColor(.red)
+            motto1
+                .prominentTitle()
+            motto2
+                .foregroundColor(.blue)
+            CapsuleText(text: "Test")
+                .font(Font.body)
+            CapsuleText(text: "Test 2")
+                .font(.caption)
+                .foregroundColor(.red)
+        }
     }
 }
 
