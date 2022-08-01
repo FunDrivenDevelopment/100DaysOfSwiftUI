baeddcfdd0ea469b2430363bd25332048f914663
JiWon Lee <jiwon.swdev@gmail.com>
2022-08-01 23:44:25 +0900

    animating bindings

--- Animation/rieul/Animtation/Animtation/ContentView.swift
+++ Animation/rieul/Animtation/Animtation/ContentView.swift
@@ -9,28 +9,28 @@ import SwiftUI
 
 struct ContentView: View {
     @State private var animationAmount: Double = 1.0
-
+    
     var body: some View {
-        Button("Tap Me") {
-//            animationAmount += 1
-        }
-        .padding(50)
-        .background(.red)
-        .foregroundColor(.white)
-        .clipShape(Circle())
-        .overlay(
-            Circle()
-                .stroke(.red)
-                .scaleEffect(animationAmount)
-                .opacity(2 - animationAmount)
-                .animation(
+        VStack {
+            Stepper(
+                "Scale amount",
+                value: $animationAmount.animation(
                     .easeInOut(duration: 1)
-                        .repeatForever(autoreverses: false),
-                    value: animationAmount
-                )
-        )
-        .onAppear {
-            animationAmount = 2
+                        .repeatCount(3, autoreverses: true)
+                ),
+                in: 1...10
+            )
+            
+            Spacer()
+            
+            Button("Tap Me") {
+                animationAmount += 1
+            }
+            .padding(50)
+            .background(.red)
+            .foregroundColor(.white)
+            .clipShape(Circle())
+            .scaleEffect(animationAmount)
         }
     }
 }
