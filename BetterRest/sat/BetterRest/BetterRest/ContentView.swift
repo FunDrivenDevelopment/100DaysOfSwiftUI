1c7e9d299204d46ed4e3b7011afa41439275b990
Yeonu Yi <yeonu0616@gmail.com>
2022-08-03 17:59:36 +0100

    🔧 Change VStack to Section

--- BetterRest/sat/BetterRest/BetterRest/ContentView.swift
+++ BetterRest/sat/BetterRest/BetterRest/ContentView.swift
@@ -27,26 +27,26 @@ struct ContentView: View {
     var body: some View {
         NavigationView {
             Form {
-                VStack(alignment: .leading, spacing: 0) {
-                    Text("When do you want to wake up?")
-                        .font(.headline)
-
+                Section {
                     DatePicker("Please enter a time", selection: $wakeUp, displayedComponents: .hourAndMinute)
                         .labelsHidden()
+                } header: {
+                    Text("When do you want to wake up?")
+                        .font(.headline)
                 }
 
-                VStack(alignment: .leading, spacing: 0) {
+                Section {
+                    Stepper("\(sleepAmount.formatted()) hours", value: $sleepAmount, in: 4...12, step: 0.25)
+                } header: {
                     Text("Desired amount of sleep")
                         .font(.headline)
-
-                    Stepper("\(sleepAmount.formatted()) hours", value: $sleepAmount, in: 4...12, step: 0.25)
                 }
 
-                VStack(alignment: .leading, spacing: 0) {
+                Section {
+                    Stepper(coffeeAmount == 1 ? "1 cup" : "\(coffeeAmount) cups", value: $coffeeAmount, in: 1...20)
+                } header: {
                     Text("Daily coffee intake")
                         .font(.headline)
-
-                    Stepper(coffeeAmount == 1 ? "1 cup" : "\(coffeeAmount) cups", value: $coffeeAmount, in: 1...20)
                 }
             }
             .navigationTitle("Better Rest")
