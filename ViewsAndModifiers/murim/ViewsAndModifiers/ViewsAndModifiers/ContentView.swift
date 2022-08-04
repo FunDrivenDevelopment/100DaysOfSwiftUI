61c74005d7d961cf252c6a54ce0882ddb4bc8efa
nora <anes2354@gmail.com>
2022-08-04 14:41:06 +0900

    중복 피커

--- ViewsAndModifiers/murim/ViewsAndModifiers/ViewsAndModifiers/ContentView.swift
+++ ViewsAndModifiers/murim/ViewsAndModifiers/ViewsAndModifiers/ContentView.swift
@@ -8,7 +8,10 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
+    @State var useRedText = false
+    @State var selectPercent = false
+    
+    var nestingView: some View {
         Text("Hello, world!")
             .padding()
                 .background(.red)
@@ -19,7 +22,31 @@ struct ContentView: View {
                 .padding()
                 .background(.yellow)
     }
+    
+    var toggleView : some View {
+        Button("change Text Color"){
+            useRedText.toggle()
+        }
+        .foregroundColor(useRedText ? .red : .blue)
+    }
+    
+    var wesplitConditional: some View {
+        Picker("tip percentage", selection: $selectPercent) {
+            ForEach(0..<101) {
+                Text("\($0) %")
+                    .foregroundColor($0 == 0 ? .red : .blue )
+            }
+        }
+        .pickerStyle(.wheel)
+    }
+    
+    var body: some View {
+        toggleView
+        wesplitConditional
+    }
 }
+
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
