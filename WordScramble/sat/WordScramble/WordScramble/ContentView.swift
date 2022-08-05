7c4678db1d7f035904a525a0db2259b0f553e1e7
Yeonu Yi <yeonu0616@gmail.com>
2022-08-05 19:31:51 +0100

    ✨ Use start.txt

--- WordScramble/sat/WordScramble/WordScramble/ContentView.swift
+++ WordScramble/sat/WordScramble/WordScramble/ContentView.swift
@@ -31,6 +31,7 @@ struct ContentView: View {
             }
             .navigationTitle(rootWord)
             .onSubmit(addNewWord)
+            .onAppear(perform: startGame)
         }
     }
 
@@ -44,6 +45,18 @@ struct ContentView: View {
             usedWords.insert(answer, at: 0)
         }
         newWord = ""
+    }
+
+    func startGame() {
+        if let startWordsURL = Bundle.main.url(forResource: "start", withExtension: "txt") {
+            if let startWords = try? String(contentsOf: startWordsURL) {
+                let allWords = startWords.components(separatedBy: "\n")
+                rootWord = allWords.randomElement() ?? "silkworm"
+                return
+            }
+        }
+
+        fatalError("Could not load start.txt from bundle.")
     }
 }
 
