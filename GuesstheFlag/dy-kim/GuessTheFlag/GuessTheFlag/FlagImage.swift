fbad4c641ac8f7556d879a9faced3b0026e3b3d8
Daeyoung Kim <daykim22@gmail.com>
2022-08-11 20:56:16 +0900

    Project #6 챌린지 완료: Guess The Flag 프로젝트에 애니메이션 추가하기

--- GuesstheFlag/dy-kim/GuessTheFlag/GuessTheFlag/FlagImage.swift
+++ GuesstheFlag/dy-kim/GuessTheFlag/GuessTheFlag/FlagImage.swift
@@ -9,9 +9,11 @@ import SwiftUI
 
 struct FlagImage: View {
     let image: String
+    let state: FlagState
 
-    init(_ image: String) {
+    init(_ image: String, state: FlagState = .initial) {
         self.image = image
+        self.state = state
     }
 
     var body: some View {
@@ -19,7 +21,16 @@ struct FlagImage: View {
             .renderingMode(.original)
             .clipShape(RoundedRectangle(cornerRadius: 10))
             .shadow(radius: 5)
+            .rotation3DEffect(.degrees(self.state == .tapped ? 360 : 0),
+                              axis: (0, 1, 0))
+            .opacity(self.state == .notTapped ? 0.25 : 1)
+            .scaleEffect(self.state == .notTapped ? 0.75 : 1)
+            .animation(.spring(), value: state)
     }
+}
+
+enum FlagState {
+    case initial, tapped, notTapped
 }
 
 struct FlagImage_Previews: PreviewProvider {
