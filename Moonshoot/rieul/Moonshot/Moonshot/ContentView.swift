0bcabb6ea80d41b7971b16d5b0aa9d1cfdaa008e
JiWon Lee <jiwon.swdev@gmail.com>
2022-08-14 14:29:39 +0900

    how scrollview lets us work with scrolling data

--- Moonshoot/rieul/Moonshot/Moonshot/ContentView.swift
+++ Moonshoot/rieul/Moonshot/Moonshot/ContentView.swift
@@ -7,14 +7,29 @@
 
 import SwiftUI
 
+struct CustomText: View {
+    let text: String
+
+    var body: some View {
+        Text(text)
+    }
+
+    init(_ text: String) {
+        print("Creating a new CustomText")
+        self.text = text
+    }
+}
+
 struct ContentView: View {
     var body: some View {
-        GeometryReader { geo in
-            Image("moon")
-                .resizable()
-                .scaledToFit()
-                .frame(width: geo.size.width * 0.8)
-                .frame(width: geo.size.width, height: geo.size.height)
+        ScrollView(.horizontal) {
+            LazyHStack(spacing: 10) {
+                ForEach(0..<100) {
+                    CustomText("Item \($0)")
+                        .font(.title)
+                }
+            }
+            .frame(maxWidth: .infinity)
         }
     }
 }
