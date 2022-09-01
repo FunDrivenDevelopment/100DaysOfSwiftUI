0ecc1ce507cccd1c11fc025408be44c1f997b280
Daeyoung Kim <daykim22@gmail.com>
2022-09-01 11:07:27 +0900

    Project #8 - 프로젝트 폴더 이름 수정 & 프로젝트 생성

---
+++ Moonshot/dy-kim/Moonshot/Moonshot/MoonshotApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoonshotApp.swift
+//  Moonshot
+//
+//  Created by 김대영 on 2022/09/01.
+//
+
+import SwiftUI
+
+@main
+struct MoonshotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
