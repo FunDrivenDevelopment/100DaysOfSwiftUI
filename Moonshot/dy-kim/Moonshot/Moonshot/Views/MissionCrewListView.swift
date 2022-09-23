8b5b4a1c4fdafeebf82281184c2a0fdb8d3eff54
Daeyoung Kim <daykim22@gmail.com>
2022-09-23 14:40:05 +0900

    MissionCrewListView 추가

---
+++ Moonshot/dy-kim/Moonshot/Moonshot/Views/MissionCrewListView.swift
@@ -0,0 +1,47 @@
+//
+//  MissionCrewListView.swift
+//  Moonshot
+//
+//  Created by 김대영 on 2022/09/23.
+//
+
+import SwiftUI
+
+struct MissionCrewListView: View {
+    let crews: [MissionCrew]
+
+    var body: some View {
+        ScrollView(.horizontal, showsIndicators: false) {
+            HStack{
+                ForEach(self.crews) { crew in
+                    HStack {
+                        Image(crew.id)
+                            .resizable()
+                            .frame(width: 104, height: 72)
+                            .clipShape(RoundedRectangle(cornerRadius: 20))
+                            .overlay {
+                                RoundedRectangle(cornerRadius: 20)
+                                    .strokeBorder(.thickMaterial, lineWidth: 2)
+                            }
+
+                        VStack(alignment: .leading, spacing: 5) {
+                            Text(crew.name)
+                                .font(.headline.bold())
+                                .foregroundStyle(.primary)
+
+                            Text(crew.role)
+                                .foregroundStyle(.secondary)
+                        }
+                    }
+                    .padding(.horizontal)
+                }
+            }
+        }
+    }
+}
+
+struct MissionCrewListView_Previews: PreviewProvider {
+    static var previews: some View {
+        MissionCrewListView(crews: missionCrews)
+    }
+}
