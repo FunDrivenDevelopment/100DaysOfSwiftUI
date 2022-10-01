d0a4882f231267e9fac51946276ce6800c9ade07
JiWon Lee <jiwon.swdev@gmail.com>
2022-10-01 13:59:37 +0900

    init project

---
+++ HabitTracking/rieul/HabitTracking/HabitTracking/HabitTrackingApp.swift
@@ -0,0 +1,17 @@
+//
+//  HabitTrackingApp.swift
+//  HabitTracking
+//
+//  Created by 이지원 on 2022/10/01.
+//
+
+import SwiftUI
+
+@main
+struct HabitTrackingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
